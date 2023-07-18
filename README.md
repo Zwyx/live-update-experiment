e3d53ff9af72195407046d9bef532d7901b7e704
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-07-18 23:30:02 +0800

    Set up Ably

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # live-update-experiment
 
-Uses a scheduled GitHub Action pushing messages to an [Ably](https://ably.com/) channel, to which a [Vite React app](live-update-experiment-zwyx.netlify.app) is subscribed.
+Uses a scheduled GitHub Action pushing messages to an [Ably](https://ably.com/) channel, to which a [Vite React app](https://zwyx.github.io/live-update-experiment/) is subscribed.
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-	return <div />;
-}
+import { configureAbly, useChannel } from "@ably-labs/react-hooks";
+import { useState } from "react";
 
-export default App;
+// Do not use this authentication method for production;
+// https://ably.com/docs/auth#token-authentication
+configureAbly({ key: import.meta.env.VITE_ABLY_API_KEY });
+
+export const App = () => {
+	const [messages, updateMessages] = useState<string[]>([]);
+
+	const [channel] = useChannel("live-update-experiment", (message) => {
+		updateMessages((prev) => [...prev, message.data]);
+	});
+
+	return (
+		<div>
+			{channel.state}{" "}
+			{messages.map((message, i) => (
+				// Can use index as key because existing elements will never change
+				<div key={i}>{message}</div>
+			))}
+		</div>
+	);
+};
