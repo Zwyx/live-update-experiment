1a04aa68fa216a34bf51eec1c55b41d31a1f0202
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-07-19 00:27:25 +0800

    Make it pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { configureAbly, useChannel } from "@ably-labs/react-hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Confetti } from "~/lib/confetti.min.js";
 
 // Do not use this authentication method for production;
 // https://ably.com/docs/auth#token-authentication
@@ -12,13 +13,61 @@ export const App = () => {
 		updateMessages((prev) => [...prev, message.data]);
 	});
 
+	useEffect(() => {
+		const confetti = new Confetti("confetti");
+
+		confetti.setCount(75);
+		confetti.setSize(1);
+		confetti.setPower(25);
+		confetti.setFade(false);
+		confetti.destroyTarget(false);
+	}, []);
+
+	useEffect(() => {
+		if (messages.length) {
+			const confettiElement = document.getElementById("confetti");
+
+			if (confettiElement) {
+				const boundingClientRect = confettiElement.getBoundingClientRect();
+
+				confettiElement.dispatchEvent(
+					new PointerEvent("click", {
+						clientX: boundingClientRect.x + boundingClientRect.width / 2,
+						clientY: boundingClientRect.y,
+					}),
+				);
+			}
+		}
+	}, [messages]);
+
 	return (
-		<div>
-			{channel.state}{" "}
-			{messages.map((message, i) => (
-				// Can use index as key because existing elements will never change
-				<div key={i}>{message}</div>
-			))}
-		</div>
+		<>
+			<div className="m-8 border rounded p-4 max-w-lg flex justify-between mx-auto">
+				<div>Channel state:</div>
+				<div className="text-blue-500">{channel.state}</div>
+			</div>
+
+			<div className="m-8 border rounded p-4 max-w-lg mx-auto">
+				{messages.map((message, i) => (
+					// Can use index as key because existing elements will never change
+					<div key={i} className="mb-4 font-mono">
+						- {message}
+					</div>
+				))}
+
+				{!messages.length && (
+					<div className="text-gray-500 text-center mb-4">
+						(Waiting for messages)
+					</div>
+				)}
+
+				<span className="relative flex h-3 w-3 m-auto">
+					<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75"></span>
+					<span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+				</span>
+
+				<div id="confetti" />
+			</div>
+		</>
 	);
 };
